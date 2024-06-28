51d209ea1651b54a58dd220a8b2b0258a17c8b74
Slug-Boi <theis.p.holm@gmail.com>
2024-06-28 14:52:11 +0200

    feat: cleaned up the listener functions and added ent db to check loop

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/albe2669/spotify-viewer/ent"
+	"github.com/albe2669/spotify-viewer/ent/schema/pulid"
 	"github.com/albe2669/spotify-viewer/generated"
 	"github.com/albe2669/spotify-viewer/resolver"
 	"github.com/albe2669/spotify-viewer/utils"
@@ -75,9 +76,14 @@ func getPlayer(sa *spotify.Spotify) echo.HandlerFunc {
 // Global playerstate variable
 var playerState *PlayerState
 
-func playerStateLoop(sa *spotify.Spotify) {
+func playerStateLoop(sa *spotify.Spotify, dbClient *ent.Client) {
+	// Sleep for 5 seconds to give the server time to start
+	time.Sleep(5 * time.Second)
+
 	//TODO: Add a loop restart if error occours
 	ctx := context.Background()
+
+	defer dbClient.Close()
 
 	// This is the initial playerstate
 	playerState = &PlayerState{}
@@ -88,22 +94,37 @@ func playerStateLoop(sa *spotify.Spotify) {
 			// fails
 			utils.Logger.Error("Error getting playerstate", zap.Error(err))
 		}
-		if player != nil {
-			*playerState = updatePlayerState(player)
+
+		// Check if player is not nil and that the player has an item
+		if player != nil && player.Item != nil {
+
+			// Create a track from the playerstate
+			track := makeTrack(player)
+
+			// This function requires data from the previous loop so it needs to be called before the update to the playerstate
+			// This is to check if the track has changed and if so add it to the db or if the track has been replayed
+			dbCheckUpdate(dbClient, track, player.Progress, ctx)
+
+			// This function updates the playerstate with the new track and progress 
+			playerState = updatePlayerState(track, player.Progress)
 		}
 
 		// For testing to see if the loop is working
-		utils.Logger.Info("Playerstate recieved", zap.Any("player", playerState))
+		//utils.Logger.Info("Playerstate recieved", zap.Any("player", playerState))
+		// Debugging Query to see if the tracks are being added to the db correctly 
+		// Best to use len since it removes some of the clutter from the log
+		tr, err := dbClient.Track.Query().All(ctx)
+		if err != nil {
+			utils.Logger.Error("Error querying tracks", zap.Error(err))
+		}
+		utils.Logger.Info("Tracks", zap.Any("tracks", len(tr)))
 
 		time.Sleep(5 * time.Second)
 	}
-
 }
 
-func updatePlayerState(player *spotifyLib.PlayerState) PlayerState {
-	if player.Item == nil {
-		return *playerState
-	}
+func makeTrack(player *spotifyLib.PlayerState) *ent.Track {
+	// Check if playerState is active if not return previous track as fallback
 
 	artists := make([]string, len(player.Item.Artists))
 	//TODO: For some reason the genres field is not available
@@ -115,7 +136,6 @@ func updatePlayerState(player *spotifyLib.PlayerState) PlayerState {
 	for i, artist := range player.Item.Artists {
 		artists[i] = artist.Name
 	}
-	//TODO: Move to a separate function
 	track := &ent.Track{
 		Name:          player.Item.Name,
 		Artists:       artists,
@@ -126,25 +146,54 @@ func updatePlayerState(player *spotifyLib.PlayerState) PlayerState {
 		URI:           string(player.Item.URI),
 	}
 
-	// Check if the track has changed
-	dbCheckUpdate(track, player.Progress)
+	// Return a pointer to a track
+	return track
 
-	//TODO: not sure if this is how you do pointer updates in Golang
-	ps := &PlayerState{*track, player.Progress, time.Now()}
-	return *ps
 }
 
-func dbCheckUpdate(track *ent.Track, progress int) {
+func updatePlayerState(track *ent.Track, progress int) *PlayerState {
+	if track.Name == "" {
+		return playerState
+	}
+
+	//TODO: not sure if this is how you do pointer updates in Golang
+	ps := &PlayerState{*track, progress, time.Now()}
+	return ps
+}
+
+func dbCheckUpdate(dbClient *ent.Client, track *ent.Track, progress int, ctx context.Context) {
+
+	// Check if the track has just changed and if so add it to the db	
 	if playerState.Track.Name != track.Name {
-		//TODO: add track to db using ent
+		addTrack(dbClient, track, ctx)
 	}
 
 	// Check for replays
 	// TODO: Maybe find a better way to do this but works for now
-	if (int(track.DurationMs /50) * 100) < playerState.Progress && progress <= int((track.DurationMs/05) * 100) {
-		//TODO: add track to db using ent
+	if (int(track.DurationMs/50)*100) < playerState.Progress && progress <= int((track.DurationMs/05)*100) {
+		addTrack(dbClient, track, ctx)
 	}
 
+}
+
+func addTrack(dbClient *ent.Client, track *ent.Track, ctx context.Context) {
+	// NOTE: AritstGenres is not implemented yet so it will be nil see makeTrack function for more info
+	_, err := dbClient.Track.Create().
+		SetName(track.Name).
+		SetArtists(track.Artists).
+		SetTrackID(pulid.MustNew(track.Name)).
+		SetArtistsGenres(track.ArtistsGenres).
+		SetAlbumName(track.AlbumName).
+		SetAlbumImageURI(track.AlbumImageURI).
+		SetDurationMs(track.DurationMs).
+		SetURI(track.URI).
+		SetCreatedAt(time.Now()).
+		Save(ctx)
+
+	if err != nil {
+			utils.Logger.Error("Error creating track", zap.Error(err))
+	}
+	
 }
 
 func httpServer(graphqlServer *handler.Server) *echo.Echo {
@@ -174,7 +223,6 @@ func connectDatabase() *ent.Client {
 	if err != nil {
 		log.Fatalf("failed opening connection to sqlite: %v", err)
 	}
-	defer client.Close()
 	ctx := context.Background()
 	// Run the automatic migration tool to create all schema resources.
 	if err := client.Schema.Create(ctx); err != nil {
@@ -196,7 +244,8 @@ func main() {
 		utils.Logger.Fatal("failed reading config", zap.Error(err))
 	}
 
-	// dbClient := connectDatabase()
+	//TODO: Should this be a pointer instead so that we can do reconnects later down the line?
+	dbClient := connectDatabase()
 	// tracks, _ := dbClient.Track.Query().All(context.Background())
 	//log.Println("Tracks:", tracks)
 
@@ -207,7 +256,7 @@ func main() {
 	spotify.SetupRoutes(e)
 	spotify.Authenticate()
 
-	playerStateLoop(spotify)
+	go playerStateLoop(spotify, dbClient)
 	e.GET("/player", getPlayer(spotify))
 
 	e.Logger.Fatal(e.Start(fmt.Sprintf("%s:%d", config.Server.Host, config.Server.Port)))
