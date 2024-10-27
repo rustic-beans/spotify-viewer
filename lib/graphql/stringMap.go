742d5b307bc4fa13cb4c466ee560f8c9bd2af8ec
Albert Rise Nielsen <albert@risenielsen.dk>
2024-10-27 23:05:38 +0100

    refactor(stringMap): change marshalling into context marshaller to allow errors

--- lib/graphql/stringMap.go
+++ lib/graphql/stringMap.go
@@ -1,47 +1,47 @@
 package graphql
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
-
-	"github.com/99designs/gqlgen/graphql"
 )
 
 // StringMap represents a map[string]string type for GraphQL
 type StringMap map[string]string
 
-// MarshalStringMap marshals the map[string]string to JSON
-func MarshalStringMap(m map[string]string) graphql.Marshaler {
-	return graphql.WriterFunc(func(w io.Writer) {
-		json.NewEncoder(w).Encode(m)
-	})
+func (s StringMap) MarshalGQLContext(ctx context.Context, w io.Writer) error {
+	if err := json.NewEncoder(w).Encode(s); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-// UnmarshalStringMap unmarshals JSON to a map[string]string
-func UnmarshalStringMap(v interface{}) (map[string]string, error) {
+func (s *StringMap) UnmarshalGQLContext(ctx context.Context, v interface{}) error {
 	switch v := v.(type) {
 	case map[string]string:
-		return v, nil
+		*s = v
 	case map[string]interface{}:
 		result := make(map[string]string)
 
 		for k, val := range v {
 			str, ok := val.(string)
 			if !ok {
-				return nil, fmt.Errorf("value for key %s is not a string", k)
+				return fmt.Errorf("value for key %s is not a string", k)
 			}
 
 			result[k] = str
 		}
 
-		return result, nil
+		*s = result
 	case string:
-		var result map[string]string
-		err := json.Unmarshal([]byte(v), &result)
-
-		return result, err
+		if err := json.Unmarshal([]byte(v), &s); err != nil {
+			return err
+		}
 	default:
-		return nil, fmt.Errorf("cannot unmarshal %T into map[string]string", v)
+		return fmt.Errorf("cannot unmarshal %T into map[string]string", v)
 	}
+
+	return nil
 }
