7ef2d881cd56462df32f2930a7071f6f09c330d5
Albert Rise Nielsen <albert@risenielsen.dk>
2024-10-27 23:31:48 +0100

    refactor: fix playerState lint errors

--- lib/spotify/playerstate.go
+++ lib/spotify/playerstate.go
@@ -2,10 +2,8 @@ package spotify
 
 import (
 	"context"
-	"net/http"
 	"time"
 
-	"github.com/labstack/echo/v4"
 	"github.com/rustic-beans/spotify-viewer/ent"
 	"github.com/rustic-beans/spotify-viewer/ent/schema/pulid"
 	"github.com/rustic-beans/spotify-viewer/utils"
@@ -14,29 +12,24 @@ import (
 	spotifyLib "github.com/zmb3/spotify/v2"
 )
 
+const (
+	sleepTime                     = 5 * time.Second
+	lastTrackDurationPercentage   = 50
+	replayTrackDurationPercentage = 5
+)
+
 // Global playerstate variable
 var playerState *PlayerState
 
 type PlayerState struct {
-	Track     ent.Track // This the track struct from the DB schema
-	Progress  int       `json:"progress_ms"` // This is the current progress of the track in ms
-	Date_Time time.Time // The time that the struct was updated last
-}
-
-func getPlayer(sa *Spotify) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		player, err := sa.GetPlayerState(c.Request().Context())
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, err)
-		}
-
-		return c.JSON(http.StatusOK, player)
-	}
+	Track    ent.Track // This the track struct from the DB schema
+	Progress int       `json:"progress_ms"` // This is the current progress of the track in ms
+	DateTime time.Time // The time that the struct was updated last
 }
 
 func PlayerStateLoop(sa *Spotify, dbClient *ent.Client) {
 	// Sleep for 5 seconds to give the server time to start
-	time.Sleep(5 * time.Second)
+	time.Sleep(sleepTime)
 
 	//TODO: Add a loop restart if error occours
 	ctx := context.Background()
@@ -45,6 +38,7 @@ func PlayerStateLoop(sa *Spotify, dbClient *ent.Client) {
 
 	// This is the initial playerstate
 	playerState = &PlayerState{}
+
 	for {
 		player, err := sa.GetPlayerState(ctx)
 		if err != nil {
@@ -55,35 +49,33 @@ func PlayerStateLoop(sa *Spotify, dbClient *ent.Client) {
 
 		// Check if player is not nil and that the player has an item
 		if player != nil && player.Item != nil {
-
 			// Create a track from the playerstate
 			track := makeTrack(player)
 
 			// This function requires data from the previous loop so it needs to be called before the update to the playerstate
 			// This is to check if the track has changed and if so add it to the db or if the track has been replayed
-			dbCheckUpdate(dbClient, track, player.Progress, ctx)
+			dbCheckUpdate(ctx, dbClient, track, player.Progress)
 
 			// This function updates the playerstate with the new track and progress
 			updatePlayerState(track, player.Progress)
 		}
 
 		// For testing to see if the loop is working
-		utils.Logger.Debug("Playerstate recieved", zap.Any("player", playerState))
+		utils.Logger.Debug("Playerstate receieved", zap.Any("player", playerState))
 		// Debugging Query to see if the tracks are being added to the db correctly
 		// Best to use len since it removes some of the clutter from the log
 		tr, err := dbClient.Track.Query().All(ctx)
 		if err != nil {
 			utils.Logger.Error("Error querying tracks", zap.Error(err))
 		}
+
 		utils.Logger.Debug("Tracks", zap.Any("tracks", len(tr)))
 
-		time.Sleep(5 * time.Second)
+		time.Sleep(sleepTime)
 	}
 }
 
 func makeTrack(player *spotifyLib.PlayerState) *ent.Track {
-	// Check if playerState is active if not return previous track as fallback
-
 	artists := make([]string, len(player.Item.Artists))
 	//TODO: For some reason the genres field is not available
 	// From Item.Artists despite the API saying so. This may be connected
@@ -94,6 +86,7 @@ func makeTrack(player *spotifyLib.PlayerState) *ent.Track {
 	for i, artist := range player.Item.Artists {
 		artists[i] = artist.Name
 	}
+
 	track := &ent.Track{
 		Name:          player.Item.Name,
 		Artists:       artists,
@@ -106,7 +99,6 @@ func makeTrack(player *spotifyLib.PlayerState) *ent.Track {
 
 	// Return a pointer to a track
 	return track
-
 }
 
 // since playerstate is a public variable we can just update the values inside the pointer instead of returning
@@ -117,27 +109,26 @@ func updatePlayerState(track *ent.Track, progress int) {
 
 	playerState.Track = *track
 	playerState.Progress = progress
-	playerState.Date_Time = time.Now()
+	playerState.DateTime = time.Now()
 }
 
-func dbCheckUpdate(dbClient *ent.Client, track *ent.Track, progress int, ctx context.Context) {
-
+func dbCheckUpdate(ctx context.Context, dbClient *ent.Client, track *ent.Track, progress int) {
 	// Check if the track has just changed and if so add it to the db
 	if playerState.Track.Name != track.Name {
-		addTrack(dbClient, track, ctx)
+		addTrack(ctx, dbClient, track)
 	}
 
 	// Check for replays
 	// TODO: Maybe find a better way to do this but works for now
 	// Check if last track update duration is more than 50% done and if current progress is less than 05% into the track
 	// This is what constitutes as a replay
-	if (int(track.DurationMs/50)*100) < playerState.Progress && progress <= int((track.DurationMs/05)*100) {
-		addTrack(dbClient, track, ctx)
+	if (track.DurationMs/lastTrackDurationPercentage)*100 < playerState.Progress &&
+		progress <= (track.DurationMs/replayTrackDurationPercentage)*100 {
+		addTrack(ctx, dbClient, track)
 	}
-
 }
 
-func addTrack(dbClient *ent.Client, track *ent.Track, ctx context.Context) {
+func addTrack(ctx context.Context, dbClient *ent.Client, track *ent.Track) {
 	// NOTE: AritstGenres is not implemented yet so it will be nil see makeTrack function for more info
 	_, err := dbClient.Track.Create().
 		SetName(track.Name).
@@ -154,5 +145,4 @@ func addTrack(dbClient *ent.Client, track *ent.Track, ctx context.Context) {
 	if err != nil {
 		utils.Logger.Error("Error creating track", zap.Error(err))
 	}
-
 }
