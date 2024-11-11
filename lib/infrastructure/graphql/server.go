e72ae0a3ac793670b5595f80ac2176446b33adf0
albe2669 <albert@risenielsen.dk>
2024-11-11 16:54:24 +0100

    fix: lint issues

--- lib/infrastructure/graphql/server.go
+++ lib/infrastructure/graphql/server.go
@@ -33,15 +33,18 @@ func NewServer(
 				return true // TODO: Add origin check
 			},
 		},
+		//nolint:mnd // no need to change the ping interval
 		KeepAlivePingInterval: 10 * time.Second,
 	})
 	server.AddTransport(transport.Options{})
 	server.AddTransport(transport.GET{})
 	server.AddTransport(transport.POST{})
 
+	//nolint:mnd // no need to change the cache size
 	server.SetQueryCache(lru.New(1000))
 	server.Use(extension.Introspection{})
 	server.Use(extension.AutomaticPersistedQuery{
+		//nolint:mnd // no need to change the cache size
 		Cache: lru.New(100),
 	})
 
