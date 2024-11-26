fa11b76162f2207babb3fd750a6d38887d66b108
Slug-Boi <github.unshipped609@slmail.me>
2024-11-26 21:32:30 +0100

    feat: add background component
    this is still WIP as it uses the album art as background. Needs new ent
    types before it can be finished but this will be on another branch

--- frontend/src/components/Player.vue
+++ frontend/src/components/Player.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import AlbumCover from '@/components/AlbumCover.vue';
+import Background from '@/components/Background.vue';
 import TrackInfo from '@/components/TrackInfo.vue';
 import Controls from '@/components/Controls.vue';
 import ProgressBar from '@/components/ProgressBar.vue';
@@ -26,10 +27,22 @@ const props = defineProps<{
 const playerStateObj = computed(() => useFragment(PlayerStateFragment, props.fragment));
 const trackObj = computed(() => playerStateObj.value.item);
 const imagesObj = computed(() => trackObj.value?.album);
+const backgroundObj = computed(() => trackObj.value?.album);
+//TODO: once we have the artist image from the spotify api call, we can use the obj dominant color for this
+var color = "#821271";
+
+
 </script>
 
 <template>
-  <div class="fixed inset-0 bg-gradient-to-b from-neutral-800 to-neutral-900">
+  <div class="fixed inset-0">
+    <Background class="object-cover h-full w-full "
+      v-if="backgroundObj"
+      :fragment="backgroundObj"
+    />
+    <div class="object-cover h-full w-full blur " :style="{'background-color': color}" >
+      <div class="object-cover h-full w-full bg-gradient-to-tr from-neutral-600 to-neutral-900 opacity-25 blur-lg"></div>
+    </div>
     <div class="absolute bottom-0 left-0 right-0 p-12">
       <div class="flex flex-row items-end gap-6">
         <AlbumCover
