004316079fdd28b33f1f0f6e6433578d23b856d7
Slug-Boi <github.unshipped609@slmail.me>
2024-12-05 20:16:20 +0100

    ci: try to use daggerverse go instead for easier setup

--- dagger/main.go
+++ dagger/main.go
@@ -13,17 +13,7 @@ type SpotifyViewer struct{}
 func (m *SpotifyViewer) BuildEnv(source *dagger.Directory) *dagger.Container {
 	// create a Dagger cache volume for dependencies
 	goCache := dag.CacheVolume("go-modules")
-	return dag.Container().
-		// start from a base Node.js container
-		From("golang").
-		// add the source code at /src
-		WithDirectory("/src", source).
-		// mount the cache volume at /root/.npm
-		WithMountedCache("/go-cache", goCache).
-		// change the working directory to /src
-		WithWorkdir("/src").
-		// run npm install to install dependencies
-		WithExec([]string{"go", "generate", "./ent"})
+	return dag.Go().WithModuleCache(goCache).Container().WithExec([]string{"go", "generate", "./ent"})
 }
 
 // Runs the `spotify-viewer` module tests
@@ -34,7 +24,7 @@ func (m *SpotifyViewer) Test(ctx context.Context, source *dagger.Directory) (str
 		return "", err
 	}
 
-	return output, nil
+	return "", nil
 }
 
 // Build the application container
