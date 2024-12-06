6a64594347f1ad6b956000eac6272c2316eeb895
Slug-Boi <github.unshipped609@slmail.me>
2024-12-06 14:40:34 +0100

    ci: make the publish function

--- dagger/main.go
+++ dagger/main.go
@@ -5,6 +5,9 @@ package main
 import (
 	"context"
 	"dagger/spotify-viewer/internal/dagger"
+	"fmt"
+	"math"
+	"math/rand/v2"
 )
 
 type SpotifyViewer struct{}
@@ -13,7 +16,11 @@ type SpotifyViewer struct{}
 func (m *SpotifyViewer) BuildEnv(source *dagger.Directory) *dagger.Container {
 	// create a Dagger cache volume for dependencies
 	goCache := dag.CacheVolume("go-modules")
-	return dag.Go().WithSource(source).WithModuleCache(goCache).Container().WithExec([]string{"go", "generate", "./ent"})
+	return dag.Go().
+		WithSource(source).
+		WithModuleCache(goCache).
+		Container().
+		WithExec([]string{"go", "generate", "./ent"})
 }
 
 // Runs the `spotify-viewer` module tests
@@ -30,8 +37,28 @@ func (m *SpotifyViewer) Test(ctx context.Context, source *dagger.Directory) (str
 
 // Build the application container
 // To Export please call it with ... export --path=./dist from the source folder
-func (m *SpotifyViewer) Build(source *dagger.Directory) *dagger.Directory {
+func (m *SpotifyViewer) Build_Bin(source *dagger.Directory) *dagger.Directory {
 	return m.BuildEnv(source).
 		WithExec([]string{"go", "build", "-C", "cmd", "-o", "../dist/backend"}).
 		Directory("./dist")
+}
+
+func (m *SpotifyViewer) Publish(ctx context.Context, source *dagger.Directory) (string, error) {
+	// Publish the application to the cloud
+	// test the application
+	_, err := m.Test(ctx, source)
+	if err != nil {
+		return "", err
+	}
+
+	bin := m.Build_Bin(source)
+
+	return dag.Container().From("golang:latest").
+		WithFile("/bin/backend", bin.File("backend")).
+		WithEntrypoint([]string{"/bin/backend"}).
+		WithExposedPort(8080).
+		Publish(ctx, fmt.Sprintf("ttl.sh/spotify-viewer-%.0f", math.Floor(rand.Float64()*1000000000)))
+
+	// build the application
+
 }
