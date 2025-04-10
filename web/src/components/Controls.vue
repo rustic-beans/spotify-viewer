c2d29dbbb45d69c872a63dbcf12558f08cf34871
Adrian Borup <adrian@borup.com>
2025-04-10 14:22:49 +0200

    feat(player state): Make vroom broom music haha god (#175)

--- web/src/components/Controls.vue
+++ web/src/components/Controls.vue
@@ -18,25 +18,91 @@ const isPlaying = computed(() => controlObj.value.isPlaying);
 
 <template>
   <div class="hidden md:block">
-    <button class="w-16 h-16">
-      <svg
-        v-if="isPlaying"
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-16 w-16"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-      >
-        <path d="M8 5v14l11-7z" />
-      </svg>
-      <svg
-        v-else
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-16 w-16"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-      >
-        <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
-      </svg>
-    </button>
+    <div :class="{ boxContainer: true, animated: isPlaying }">
+      <div class="box box1"></div>
+      <div class="box box2"></div>
+      <div class="box box3"></div>
+      <div class="box box4"></div>
+      <div class="box box5"></div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+@keyframes quiet {
+  25% {
+    transform: scaleY(0.6);
+  }
+  50% {
+    transform: scaleY(0.4);
+  }
+  75% {
+    transform: scaleY(0.8);
+  }
+}
+
+@keyframes normal {
+  25% {
+    transform: scaleY(1);
+  }
+  50% {
+    transform: scaleY(0.4);
+  }
+  75% {
+    transform: scaleY(0.6);
+  }
+}
+
+@keyframes loud {
+  25% {
+    transform: scaleY(1);
+  }
+  50% {
+    transform: scaleY(0.4);
+  }
+  75% {
+    transform: scaleY(1.2);
+  }
+}
+
+body {
+  display: flex;
+  justify-content: center;
+  background: black;
+  margin: 0;
+  padding: 0;
+  align-items: center;
+  height: 100vh;
+}
+
+.boxContainer {
+  margin-right: 2rem;
+  display: flex;
+  justify-content: space-between;
+  height: 4rem;
+  --boxSize: 0.5rem;
+  --gutter: 0.25rem;
+  width: calc((var(--boxSize) + var(--gutter)) * 5);
+}
+
+.box {
+  transform: scaleY(0.4);
+  height: 100%;
+  width: var(--boxSize);
+  background: #fff;
+  animation-duration: 1.2s;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: infinite;
+  border-radius: 0.5rem;
+}
+
+.boxContainer:not(.animated) .box {
+  animation-play-state: paused;
+}
+
+.animated .box1{ animation-name: quiet; }
+.animated .box2{ animation-name: normal; }
+.animated .box3{ animation-name: quiet; }
+.animated .box4{ animation-name: loud; }
+.animated .box5{ animation-name: quiet; }
+</style>
